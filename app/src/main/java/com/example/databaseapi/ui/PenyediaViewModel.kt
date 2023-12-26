52b0eadb736045a297528100a8bd6b59b50327a8
SalmanLS <salmanluthfi456@gmail.com>
2023-12-26 20:56:47 +0700

    add InsertViewModel to PenyediaViewModel

--- app/src/main/java/com/example/databaseapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/databaseapi/ui/PenyediaViewModel.kt
@@ -6,11 +6,15 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.databaseapi.KontakAplikasi
 import com.example.databaseapi.ui.home.viewmodel.HomeViewModel
+import com.example.databaseapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
     }
+
 }
 
 fun CreationExtras.aplikasiKontak() : KontakAplikasi =
